5c20971df2afed528aec44302a5b95a98c04c157
Lalit Kumar <lalitchauhan1517@gmail.com>
2023-11-23 14:18:53 +0530

    update app.js

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ passport.serializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://anonymous-vhve.onrender.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
