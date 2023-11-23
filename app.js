333b73be58edee4215bc0eefad730f83df52802f
Lalit Kumar <lalitchauhan1517@gmail.com>
2023-11-23 14:14:00 +0530

    delete server.js

--- app.js
+++ app.js
@@ -216,6 +216,6 @@ app.post("/login", function(req, res){
 
 });
 
-// app.listen(3000, function(req, res){
-//     console.log("Server started on port 3000");
-// });
+app.listen(3000, function(req, res){
+    console.log("Server started on port 3000");
+});
--- server.js
+++
@@ -1,9 +0,0 @@
-const http=require("http");
-const app=require("./app");
-
-
-const server=http.createServer(app);
-
-server.listen(3000, function(req, res){
-    console.log("Server started on port 3000");
-});
